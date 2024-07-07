3df79da36c1b9366d120054ca771001b8f6369d2
tienanhemho <tienanhemho@gmail.com>
2024-07-07 19:13:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,11 +50,11 @@ echo "City: $my_city"
 
 color_echo ${INFO} "Downloading Modules..."
 cd /root/auto-trojan/
-wget -q https://raw.githubusercontent.com/arminkz/auto-trojan/main/modules/certificate.sh
-wget -q https://raw.githubusercontent.com/arminkz/auto-trojan/main/modules/trojan.sh
-wget -q https://raw.githubusercontent.com/arminkz/auto-trojan/main/modules/nginx.sh
-wget -q https://raw.githubusercontent.com/arminkz/auto-trojan/main/modules/nginx-config.sh
-wget -q https://raw.githubusercontent.com/arminkz/auto-trojan/main/modules/decoy.sh
+wget -q https://raw.githubusercontent.com/tienanhemho/auto-trojan/main/modules/certificate.sh
+wget -q https://raw.githubusercontent.com/tienanhemho/auto-trojan/main/modules/trojan.sh
+wget -q https://raw.githubusercontent.com/tienanhemho/auto-trojan/main/modules/nginx.sh
+wget -q https://raw.githubusercontent.com/tienanhemho/auto-trojan/main/modules/nginx-config.sh
+wget -q https://raw.githubusercontent.com/tienanhemho/auto-trojan/main/modules/decoy.sh
 
 
 while [[ -z ${domain} ]]; do
